a4801111a7fc0255fc7599ff584fec0c26bc74ff
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-13 11:26:31 -0400

    update data template logic

--- weather/helper.py
+++ weather/helper.py
@@ -25,13 +25,19 @@ def cityJson(city):
     template = getJson()
     cityData = getCityData(city)
     tempProperties = template["features"][0]["properties"]
-    print(cityData["main"])
-    # for property in tempProperties:
-    #     for segment in cityData:
-    #         property = ci
-    #     print(property)
-        # property = cityData[property]
-
+    print(cityData)
     print(template["features"][0]["properties"])
-cityJson("Miami")
+
+    for properties in tempProperties:
+        for (key,value) in cityData.items():
+            if type(value) == dict:
+                for segment in value:
+                    if properties == segment :
+                        tempProperties[properties] = value[segment]
+            elif properties == key:
+                tempProperties[properties] = value
+    tempProperties["description"] = cityData["weather"][0]["description"]
+    print(tempProperties)
+
+cityJson("Madrid")
 # print(cityJson("Miami"))
