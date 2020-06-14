92ccb09476ed1818f568cd9e006b4e1dd5a6c13c
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-14 19:29:36 -0400

    home and help pages and unit error fix

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from flask import Flask,jsonify
 from weather.weather import weather
+import json
 
 app = Flask(__name__)
 app.register_blueprint(weather,url_prefix="/weather")
 
 
+@app.route("/help")
+def help():
+    with open("./json/instructions.json") as f:
+        data = json.load(f)
+    return jsonify(data)
+
 @app.route("/")
-def hello():
-    return jsonify({"about":"Hello world"})
-@app.route("/bye")
-def bye():
-    return jsonify({"about":"Bye world"})
+def home():
+    with open("./json/home.json") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return jsonify({"Error":"URL not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug = True)
--- weather/weather.py
+++ weather/weather.py
@@ -5,10 +5,13 @@ weather = Blueprint("weather",__name__)
 
 @weather.route("/")
 def home():
-    return jsonify({"about":"Welcome to the weather api"})
+    return jsonify({"about":"Welcome to the weather api add a city to get results"})
 
 @weather.route("/<city>")
 def city(city):
-    unit = request.args['unit']
+    try:
+        unit = request.args['unit']
+    except KeyError as e:
+        unit = "imperial"
     data = helper.cityJson(city,unit)
     return jsonify(data)
