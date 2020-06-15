432fcf7750364dfef5bd7947f1db8bac0d4315f4
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-15 09:27:00 -0400

    update requirements.txt

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def home():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return jsonify({"Error":"URL not found"}), 404
 
 if __name__ == '__main__':
